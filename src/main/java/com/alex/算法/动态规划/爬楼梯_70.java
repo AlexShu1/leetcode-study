d4f9b0f77dfad026a2761bc76c4c51400b42f937
shufei <shufei@jagat.com>
2025-03-05 11:36:25 +0800

    feat: LeetCode第104题:二叉树的最大深度; 核心: 通过递归, 不断从底层返回一层层的length, 最后取max.

--- src/main/java/com/alex/算法/动态规划/爬楼梯_70.java
+++ src/main/java/com/alex/算法/动态规划/爬楼梯_70.java
@@ -2,22 +2,22 @@ package com.alex.算法.动态规划;
 
 /**
  * https://leetcode.cn/problems/climbing-stairs/description/?envType=study-plan-v2&envId=top-100-liked
- *
+ * <p>
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
- *
+ * <p>
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：n = 2
  * 输出：2
  * 解释：有两种方法可以爬到楼顶。
  * 1. 1 阶 + 1 阶
  * 2. 2 阶
  * 示例 2：
- *
+ * <p>
  * 输入：n = 3
  * 输出：3
  * 解释：有三种方法可以爬到楼顶。
--- src/main/java/com/alex/算法/链表/反转链表_24.java
+++ src/main/java/com/alex/算法/链表/反转链表_24.java
@@ -12,26 +12,4 @@ public class 反转链表_24 {
         head.next = null;
         return newHead;
     }
-
-    public ListNode r2(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-        ListNode newHead = r2(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
-    }
-
-    public ListNode r3(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-        ListNode newHead = r3(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
-    }
 }
