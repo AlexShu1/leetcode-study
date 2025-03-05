e2211555454a9068463f7138d9dd0ac4a0aaf857
shufei <shufei@jagat.com>
2025-03-05 10:51:26 +0800

    feat: LeetCode第230题:二叉树中第K小的元素; 核心: 二叉搜索树的特性 + 栈的特性;

--- src/main/java/com/alex/算法/二叉树/TreeNode.java
+++ src/main/java/com/alex/算法/二叉树/TreeNode.java
@@ -18,4 +18,28 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    /**
+     * 新增方法：通过数组生成对应的二叉搜索树
+     * nums 必须要保持是升序数组
+     */
+    public static TreeNode fromSortedArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    // 辅助递归方法，用于构建BST
+    private static TreeNode buildBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;  // 防止(start + end)可能导致的整数溢出
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildBST(nums, start, mid - 1);
+        root.right = buildBST(nums, mid + 1, end);
+        return root;
+    }
 }
--- src/main/java/com/alex/算法/二叉树/二叉树中第K小的元素_230.java
+++ src/main/java/com/alex/算法/二叉树/二叉树中第K小的元素_230.java
@@ -1,34 +1,51 @@
 package com.alex.算法.二叉树;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * https://leetcode.cn/problems/kth-smallest-element-in-a-bst/description/?envType=study-plan-v2&envId=top-100-liked
- *
+ * <p>
  * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
- *
+ * <p>
  * 输入：root = [3,1,4,null,2], k = 1
  * 输出：1
  */
 public class 二叉树中第K小的元素_230 {
-    public int kthSmallest(TreeNode root, int k) {
-        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+
+    public static void main(String[] args) {
+        二叉树中第K小的元素_230 call = new 二叉树中第K小的元素_230();
+        int[] nums = new int[]{3, 1, 4, 0, 2};
+        TreeNode root = TreeNode.fromSortedArray(nums);
+        int value = call.kthSmallestV2(root, 2);
+        System.out.println(value);
+    }
+
+    /**
+     * 核心思想:
+     * 1. 因为是二叉搜索树是一个很规则的保证: 左子树 < 根节点 < 右子树
+     * 2. 因此, 小的数据都是在左子树上.
+     * 3. 通过栈的方式, 把最小的压缩在栈底. 不断的弹出最小的, 直到K==0的时候, 则到了第K小的树节点
+     */
+    public int kthSmallestV2(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
         while (root != null || !stack.isEmpty()) {
-            // 因为二叉搜索树的性质, 左子树上的元素就是最小的一系列元素(最小的是最左叶子节点.).
             while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
 
-            // 当root == null的时候, 弹出来的就是最小元素.
             root = stack.pop();
-            --k;
+            k--;
             if (k == 0) {
                 break;
             }
+
             root = root.right;
         }
+
         return root.val;
     }
 }
