ae6721bf3a24711bd46b6adcf3e92b8affa5b40c
shufei <shufei@jagat.com>
2025-03-17 18:13:27 +0800

    feat: LeetCode第405题:数字转换为十六进制数;

--- src/main/java/com/alex/算法/二分查找/二分查找.java
+++ src/main/java/com/alex/算法/二分查找/二分查找.java
@@ -16,7 +16,7 @@ public class 二分查找 {
 
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right) >> 1;
+            int mid = left + (right - left) >> 1;
             if (nums[mid] == target) {
                 return true;
             } else if (nums[mid] > target) {
